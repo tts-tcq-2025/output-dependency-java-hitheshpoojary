ba442027e635b2a306f56184befb34dfdfc56840
hitheshpoojary <67824540+hitheshpoojary@users.noreply.github.com>
2025-07-31 11:20:02 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -13,6 +13,8 @@ public class Tshirts {
         assert(size(37) == "S");
         assert(size(40) == "M");
         assert(size(43) == "L");
+        assert(size(38) == "S");
+        assert(size(42) == "M");
         System.out.println("All is well (maybe!)");
     }
 }
